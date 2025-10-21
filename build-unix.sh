f5f8b5f78bf469aaf376e64751dfd20a3c4fc9ba
Stian Lysne <stian.lysne@rakuten.com>
2025-10-21 13:22:29 -0400

    fix version error msg

--- build-unix.sh
+++ build-unix.sh
@@ -31,7 +31,7 @@ while [[ $# -gt 0 ]]; do
             if [[ "$VERSION" =~ ^([0-9]+!)?([0-9]+)(\.[0-9]+){0,2}((a|b|rc)[0-9]+)?(\.post[0-9]+)?(\.dev[0-9]+)?$ ]]; then
                 echo "Building version $VERSION"
             else
-                echo "Error: Invalid build type: '$VERSION'"
+                echo "Error: Invalid version: '$VERSION'"
                 exit 1
             fi
             shift 2
