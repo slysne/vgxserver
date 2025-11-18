fe164b10bec0f5de85e6ff9ed88e88db47225b84
Stian Lysne <stian.lysne@gmail.com>
2025-11-18 10:19:23 -0500

    added xrandom()

--- cxlib/src/include/cxmath.h
+++ cxlib/src/include/cxmath.h
@@ -217,6 +217,7 @@ int32_t strhash32( const unsigned char * data );
 uint64_t ihash64( uint64_t n );
 uint64_t ihash64v2( uint64_t n );
 uint64_t ihash64v3( uint64_t n );
+double xrandom( uint64_t state );
 int64_t hash64( const unsigned char * data, int64_t len );
 objectid_t hash128( const unsigned char *data, int64_t len );
 int64_t strhash64( const unsigned char *data );
